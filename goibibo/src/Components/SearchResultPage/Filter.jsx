909f3e2f8e922caadca64c4bb448d50f767b74d6
Shanoor24 <shanoorfb2403@gmail.com>
2021-07-23 03:20:20 +0530

    added some filters components

--- goibibo/src/Components/SearchResultPage/Filter.jsx
+++ goibibo/src/Components/SearchResultPage/Filter.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./Filter.module.css"
+import { FilterHeading } from "./FilterHeading";
+import { FilterPopular } from "./FilterPopular";
 
 function Filter() {
     return (
         <div className={styles.filter_main}>
-
+            <FilterHeading />
+            <FilterPopular />
         </div>
     )
 }
