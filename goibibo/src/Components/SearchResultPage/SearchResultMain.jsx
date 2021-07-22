364cb006d5b4744417bcc16ec89734e3dea922fd
Shanoor24 <shanoorfb2403@gmail.com>
2021-07-22 22:30:40 +0530

    added note component

--- goibibo/src/Components/SearchResultPage/SearchResultMain.jsx
+++ goibibo/src/Components/SearchResultPage/SearchResultMain.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Note } from "./Note";
 import { SearchBar } from "./SearchBar";
 
 function SearchResultMain() {
     return (
         <div>
             <SearchBar />
+            <Note />
             <div style={{height:"1000px"}}>
 
             </div>
