8f4aaa98687c44480ae910b482981e270c4d4d80
Shanoor24 <shanoorfb2403@gmail.com>
2021-07-22 16:24:42 +0530

    Search Bar added

---
+++ goibibo/src/Components/SearchResultPage/SearchResultMain.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { SearchBar } from "./SearchBar";
+
+function SearchResultMain() {
+    return (
+        <div>
+            <SearchBar />
+            <div style={{height:"1000px"}}>
+
+            </div>
+        </div>
+    )
+}
+
+export default SearchResultMain;
